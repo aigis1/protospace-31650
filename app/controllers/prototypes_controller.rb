04d715d2f62d5eafaa22414df9bc3b328db2c496
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-11-04 15:59:23 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,58 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(proto_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    move_to_index
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(proto_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def proto_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user ==  @prototype.user
+      redirect_to root_path
+    end
+  end
+
+end
